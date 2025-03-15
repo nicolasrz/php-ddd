3247194ae0e030bff74338ea959d0dbfdce6d212
NicolasRz <nicolasruiz.pro@gmail.com>
2025-03-15 20:49:36 +0100

    remove useless class

--- src/index.php
+++ src/index.php
@@ -4,14 +4,12 @@ include __DIR__ . "/../vendor/autoload.php";
 
 use App\Core\DependencyInjection\Container;
 use App\Core\Routing\Router;
-use App\Core\Routing\Middleware;
 use App\Core\Security\SecurityMiddleware;
 session_start();
 
 $container = new Container();
 $securityMiddleware = $container->get(SecurityMiddleware::class);
-$securityMiddleware->handle();  
+$securityMiddleware->handle();
 $router = new Router($container);
 
-// new Middleware();
 $router->dispatch();
